4027d45b5f59950fb8f2edb89c20f7d5d18d6340
CarlosPODN <carlosportillodn@gmail.com>
2026-07-21 17:40:42 -0600

    project scaffold

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex min-h-svh items-center justify-center bg-bg-primary text-text-primary">
+      <p className="font-sans text-lg">Expense Splitter</p>
+    </main>
+  )
+}
+
+export default App
